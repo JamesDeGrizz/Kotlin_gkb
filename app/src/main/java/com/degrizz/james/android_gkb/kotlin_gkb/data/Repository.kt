1efe0e594391b5856d453fc948bd7b81dff438dc
JamesDeGrizz <xoce90@mail.ru>
2021-01-24 20:16:48 +0300

    finish #2

--- app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/data/Repository.kt
+++ app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/data/Repository.kt
@@ -3,36 +3,36 @@ package com.degrizz.james.android_gkb.kotlin_gkb.data
 import com.degrizz.james.android_gkb.kotlin_gkb.data.model.Note
 
 object Repository {
-    private val notes: List<Note>
-
-    init {
-        notes = listOf(
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xfff06292.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xff9575cd.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xff64b5f6.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xff4db6ac.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xffb2ff59.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xffffeb3b.toInt()),
-            Note("Моя первая заметка",
-                "Kotlin очень краткий, но при этом выразительный язык",
-                0xffff6e40.toInt())
-        )
-    }
-
-    fun getNotes(): List<Note> {
-        return notes
-    }
-
+    private val notes: List<Note> = listOf(
+        /*
+        * Note(Resources.getSystem().getString(R.string.noteTitle),
+            Resources.getSystem().getString(R.string.noteContent),
+            0xfff06292.toInt())
+            falls with java.lang.ExceptionInInitializerError
+            ......
+            Caused by: android.content.res.Resources$NotFoundException: String resource ID #0x7f0e0050
+            idk why
+        * */
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xfff06292.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xff9575cd.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xff64b5f6.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xff4db6ac.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xffb2ff59.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xffffeb3b.toInt()),
+        Note("Моя первая заметка",
+            "Kotlin очень краткий, но при этом выразительный язык",
+            0xffff6e40.toInt())
+    )
 }
--- app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainActivity.kt
+++ app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainActivity.kt
@@ -3,24 +3,26 @@ package com.degrizz.james.android_gkb.kotlin_gkb.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.degrizz.james.android_gkb.kotlin_gkb.R
+import androidx.lifecycle.ViewModelProvider
+import com.degrizz.james.android_gkb.kotlin_gkb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ui: ActivityMainBinding
     lateinit var viewModel: MainViewModel
     lateinit var adapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)//Не забудьте поменять в Стилях приложения тему на Theme.AppCompat.Light.NoActionBar
+        ui = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ui.root)
+        setSupportActionBar(ui.toolbar)
 
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         adapter = MainAdapter()
-        mainRecycler.adapter = adapter
+        ui.mainRecycler.adapter = adapter
 
-        viewModel.viewState().observe(this, Observer<MainViewState> { t ->
-            t?.let { adapter.notes = it.notes }
+        viewModel.viewState().observe(this, Observer<MainViewState> { state ->
+            state?.let { adapter.notes = state.notes }
         })
     }
 }
