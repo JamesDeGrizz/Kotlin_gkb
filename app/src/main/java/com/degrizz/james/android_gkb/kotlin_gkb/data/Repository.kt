f9e08d92b2ef894c2c14b875487e5c035145bd57
JamesDeGrizz <xoce90@mail.ru>
2021-01-25 10:14:12 +0300

    review fixes #2

--- app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/data/Repository.kt
+++ app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/data/Repository.kt
@@ -4,15 +4,6 @@ import com.degrizz.james.android_gkb.kotlin_gkb.data.model.Note
 
 object Repository {
     val notes: List<Note> = listOf(
-        /*
-        * Note(Resources.getSystem().getString(R.string.noteTitle),
-            Resources.getSystem().getString(R.string.noteContent),
-            0xfff06292.toInt())
-            falls with java.lang.ExceptionInInitializerError
-            ......
-            Caused by: android.content.res.Resources$NotFoundException: String resource ID #0x7f0e0050
-            idk why
-        * */
         Note("Моя первая заметка",
             "Kotlin очень краткий, но при этом выразительный язык",
             0xfff06292.toInt()),
