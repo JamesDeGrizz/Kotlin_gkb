05fbedf68fd1c1b9b2f0d81c7b9690863763f47a
JamesDeGrizz <xoce90@mail.ru>
2021-01-31 23:08:52 +0300

    review fixes #3

--- app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainViewModel.kt
+++ app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainViewModel.kt
@@ -13,7 +13,6 @@ class MainViewModel : ViewModel() {
         Repository.getNotes().observeForever { notes ->
             viewStateLiveData.value = viewStateLiveData.value?.copy(notes = notes) ?: MainViewState(notes)
         }
-        viewStateLiveData.value = MainViewState(Repository.notes)
     }
 
     fun viewState(): LiveData<MainViewState> = viewStateLiveData
