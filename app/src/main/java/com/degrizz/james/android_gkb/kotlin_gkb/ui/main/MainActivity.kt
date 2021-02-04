2037ba6934764c1640e8f7eb0487c66e5245780d
JamesDeGrizz <xoce90@mail.ru>
2021-02-05 02:00:33 +0300

    finish hw #5

--- app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainActivity.kt
+++ app/src/main/java/com/degrizz/james/android_gkb/kotlin_gkb/ui/main/MainActivity.kt
@@ -3,13 +3,17 @@ package com.degrizz.james.android_gkb.kotlin_gkb.ui.main
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import com.degrizz.james.android_gkb.kotlin_gkb.R
 import com.degrizz.james.android_gkb.kotlin_gkb.data.model.Note
 import com.degrizz.james.android_gkb.kotlin_gkb.databinding.ActivityMainBinding
+import com.firebase.ui.auth.AuthUI
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
+class MainActivity : BaseActivity<List<Note>?, MainViewState>(), LogoutDialog.LogoutListener {
     override val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
     override val ui: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override val layoutRes: Int = R.layout.activity_main
@@ -34,6 +38,20 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
         adapter.notes = data
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean =
+        MenuInflater(this).inflate(R.menu.menu_main, menu).let { true }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when(item.itemId) {
+            R.id.logout -> showLogoutDialog().let { true }
+            else -> false
+        }
+
+    private fun showLogoutDialog() {
+        supportFragmentManager.findFragmentByTag(LogoutDialog.TAG) ?:
+        LogoutDialog.createInstance().show(supportFragmentManager, LogoutDialog.TAG)
+    }
+
     private fun openNoteScreen(note: Note?) {
         val intent = NoteActivity.getStartIntent(this, note?.id)
         startActivity(intent)
@@ -41,5 +59,15 @@ class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, MainActivity::class.java)
+    }
+
+    override fun onLogout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                startActivity(Intent(this, SplashActivity::class.java))
+                finish()
+
+            }
     }
 }
